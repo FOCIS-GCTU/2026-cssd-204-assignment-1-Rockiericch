e61751459c6338261bca1e8b1d27edbe47108677
Rockson Prince <rocksonprince655@gmail.com>
2026-05-30 21:05:20 +0000

    Implement initials printing in initials.py
    Added a script to print initials with a decorative pattern.

--- initials.py
+++ initials.py
@@ -1,7 +1,25 @@
+# File: intials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Rockson Boateng Prince
+# STUDENT ID:  2425400773
+# Email: 2425400773@gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Rockson Boateng Prince, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
-
-
-
-main()
+print("")
+print("...RBP")
+print("")
+print("...RRRRRRRR...........BBBBBBBB............PPPPPPPP.........")
+print("...RR......RR.........BB.......BB.........PP.......PP......")
+print("...RR.......RR........BB........BB........PP........PP.....")
+print("...RR......RR.........BB.......BB.........PP.......PP......")
+print("...RRRRRRR............BBBBBBBBB...........PPPPPPPP.........")
+print("...RR....RR...........BB.......BB.........PP...............")
+print("...RR......RR.........BB........BB........PP...............")
+print("...RR.......RR...**...BB.......BB....**...PP.............**")
+print("...RR.......RR...**...BBBBBBBB.......**...PP.............**")
+print("")
